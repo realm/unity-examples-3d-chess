235d7141ed2f3cb725d056465aafa8327c19a190
Dominic Frei <business.dominic.frei@gmail.com>
2021-08-09 10:32:16 +0100

    Add local Realm to the example template.

--- 3D Chess/Assets/Scripts/GameState.cs
+++ 3D Chess/Assets/Scripts/GameState.cs
@@ -1,3 +1,4 @@
+using Realms;
 using System.Linq;
 using UnityEngine;
 
@@ -6,14 +7,30 @@ public class GameState : MonoBehaviour
     [SerializeField] private PieceSpawner pieceSpawner = default;
     [SerializeField] private GameObject pieces = default;
 
+    private Realm realm = default;
+    private IQueryable<PieceEntity> pieceEntities = default;
+
     public void MovePiece(Piece movedPiece, Vector3 newPosition)
     {
         // Check if there is already a piece at the new position and if so, destroy it.
         var attackedPiece = FindPiece(newPosition);
         if (attackedPiece != null)
         {
+            var attackedPieceEntity = FindPieceEntity(newPosition);
+            realm.Write(() =>
+            {
+                realm.Remove(attackedPieceEntity);
+            });
             Destroy(attackedPiece.gameObject);
         }
+
+        // Update the movedPiece's RealmObject.
+        var oldPosition = movedPiece.transform.position;
+        var pieceEntity = FindPieceEntity(oldPosition);
+        realm.Write(() =>
+        {
+            pieceEntity.SetPosition(newPosition);
+        });
 
         // Update the movedPiece's GameObject.
         movedPiece.transform.position = newPosition;
@@ -27,18 +44,49 @@ public class GameState : MonoBehaviour
             Destroy(piece.gameObject);
         }
 
+        // Re-create all RealmObjects with their initial position.
+        pieceEntities = Persistence.ResetDatabase(realm);
+
         // Recreate the GameObjects.
-        pieceSpawner.CreateGameObjects(pieces);
+        CreateGameObjects();
     }
 
     private void Awake()
     {
-        pieceSpawner.CreateGameObjects(pieces);
+        realm = Realm.GetInstance();
+        pieceEntities = realm.All<PieceEntity>();
+
+        // Check if we already have PieceEntity's (which means we resume a game).
+        if (pieceEntities.Count() == 0)
+        {
+            // No game was saved, create the necessary RealmObjects.
+            pieceEntities = Persistence.ResetDatabase(realm);
+        }
+        CreateGameObjects();
+    }
+
+    private void CreateGameObjects()
+    {
+        // Each RealmObject needs a corresponding GameObject to represent it.
+        foreach (PieceEntity pieceEntity in pieceEntities)
+        {
+            PieceType type = (PieceType)pieceEntity.Type;
+            Vector3 position = pieceEntity.GetPosition();
+            pieceSpawner.SpawnPiece(type, position, pieces);
+        }
     }
 
     private Piece FindPiece(Vector3 position)
     {
         return pieces.GetComponentsInChildren<Piece>()
             .FirstOrDefault(piece => piece.transform.position == position);
+    }
+
+    private PieceEntity FindPieceEntity(Vector3 position)
+    {
+        return pieceEntities.FirstOrDefault(piece =>
+                            piece.PositionX == position.x &&
+                            piece.PositionY == position.y &&
+                            piece.PositionZ == position.z);
     }
 }
---
+++ 3D Chess/Assets/Scripts/PieceEntity.cs
@@ -0,0 +1,36 @@
+﻿using Realms;
+using UnityEngine;
+
+public class PieceEntity : RealmObject
+{
+    public int Type { get; set; }
+
+    public float PositionX { get; set; }
+    public float PositionY { get; set; }
+    public float PositionZ { get; set; }
+
+    public PieceEntity()
+    {
+
+    }
+
+    public PieceEntity(PieceType type, Vector3 position)
+    {
+        Type = (int)type;
+        PositionX = position.x;
+        PositionY = position.y;
+        PositionZ = position.z;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return new Vector3(PositionX, PositionY, PositionZ);
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        PositionX = position.x;
+        PositionY = position.y;
+        PositionZ = position.z;
+    }
+}
